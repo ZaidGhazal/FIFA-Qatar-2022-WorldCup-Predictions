482c009a00e1fd4b3c60c4816282e09fb6d85cb8
Ali Albustami <aalbustami@beyond.ai>
2022-11-26 02:18:40 +0300

    edit setup file

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ conda env create -f environment.yml
 echo "------------------ 2. Activating the Created Environment ------------------"
 eval "$(conda shell.bash hook)"
 conda activate worldcup-winner-predictor-env
+echo "Conda environment activated"
 echo "------------------ 3. Installing Pre-commit hooks ------------------"
 pre-commit install
